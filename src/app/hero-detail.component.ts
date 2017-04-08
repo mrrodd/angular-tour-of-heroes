3f0865b45c66ce819b0af615547f110a5c8cc14b
Rod McBride <rmcbride@toplinestrategies.com>
2017-04-08 10:37:42 -0700

    Add CRUD operations

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,15 +1,15 @@
 // Keep the Input import for now, you'll remove it later:
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Params }   from '@angular/router';
-import { Location }                 from '@angular/common';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 
-import { HeroService }  from './hero.service';
-import { Hero }         from "./hero";
+import { HeroService } from './hero.service';
+import { Hero } from "./hero";
 import 'rxjs/add/operator/switchMap';
 
 @Component({
-  selector: 'hero-detail',
-  templateUrl: './hero-detail.component.html',
+    selector: 'hero-detail',
+    templateUrl: './hero-detail.component.html',
 })
 
 export class HeroDetailComponent implements OnInit {
@@ -19,15 +19,20 @@ export class HeroDetailComponent implements OnInit {
         private heroService: HeroService,
         private route: ActivatedRoute,
         private location: Location
-    ) {}
+    ) { }
 
     ngOnInit(): void {
-    this.route.params
-        .switchMap((params: Params) => this.heroService.getHero(+params['id']))
-        .subscribe(hero => this.hero = hero);
+        this.route.params
+            .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+            .subscribe(hero => this.hero = hero);
     }
 
     goBack(): void {
         this.location.back();
+    }
+
+    save(): void {
+        this.heroService.update(this.hero)
+            .then(() => this.goBack());
     }
 }
